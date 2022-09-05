620e0b7c3a6a3be5743aa8865620d2933639fa97
Jumarone Matini Ito <99372820+Jumarone@users.noreply.github.com>
2022-09-06 10:42:42 +1200

    Add files via upload

---
+++ Lesson2/user_input.py
@@ -0,0 +1,34 @@
+# J Matini Ito
+# User input types
+
+# Taking input from the user
+# string = input()
+# Output
+# print(string)
+
+# taking input from the user
+# name = input("Enter your name")
+# Output
+# print (name)
+
+# taking input from the user
+# name = input("Enter your name: ")
+# Output
+# print (name)
+
+# taking input from the user as integer
+# num = int(input("Enter a number: "))
+# add = num + 1
+# Output
+# print (add)
+
+# taking input from the user as integer
+# num = float(input("Enter a number: "))
+# add = num + 1
+# Output
+# print (add)
+
+# taking input from the user as list
+# li = list(input("Enter a number: "))
+# Output
+# print(li)
